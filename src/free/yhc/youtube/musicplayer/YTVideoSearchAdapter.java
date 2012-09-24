459dcd1faa61441587fb7c38f96b5a38efc84e11
Younghyung Cho <yhcting77@gmail.com>
2012-09-24 19:46:04 +0900

    Head-License-Comemnts are added to new files.

--- src/free/yhc/youtube/musicplayer/YTVideoSearchAdapter.java
+++ src/free/yhc/youtube/musicplayer/YTVideoSearchAdapter.java
@@ -1,5 +1,24 @@
-package free.yhc.youtube.musicplayer;
+/*****************************************************************************
+ *    Copyright (C) 2012 Younghyung Cho. <yhcting77@gmail.com>
+ *
+ *    This file is part of YTMPlayer.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as
+ *    published by the Free Software Foundation either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License
+ *    (<http://www.gnu.org/licenses/lgpl.html>) for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
 
+package free.yhc.youtube.musicplayer;
 import static free.yhc.youtube.musicplayer.model.Utils.eAssert;
 
 import java.text.DateFormat;
@@ -13,6 +32,7 @@ import free.yhc.youtube.musicplayer.model.Utils;
 import free.yhc.youtube.musicplayer.model.YTFeed;
 import free.yhc.youtube.musicplayer.model.YTSearchHelper;
 import free.yhc.youtube.musicplayer.model.YTVideoFeed;
+
 
 public class YTVideoSearchAdapter extends YTSearchAdapter {
     // Flags
